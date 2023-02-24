48502ee3337688f834a41b1f7a0203c9be164060
Supriya Bhurle <83913939+Supriya-Bhurle@users.noreply.github.com>
2023-02-24 11:01:44 +0530

    Added : sumavgmincount operations on DB

--- src/main/java/com/bridgelabz/EmployeePayroll.java
+++ src/main/java/com/bridgelabz/EmployeePayroll.java
@@ -1,6 +1,18 @@
 package com.bridgelabz;
 
 import java.sql.*;
+import java.util.Enumeration;
+
+public class EmployeePayroll {
+    public static void CreatedConnection() {
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/payroll_service", "root",
+                    "Root");
+            Statement st = conn.createStatement();
+            st.execute("select sum(Salary) from employee_payroll");
+            st.execute("select avg(Salary) from employee_payroll");
+            st.execute("select min(Salary) from employee_payroll");
+            st.execute("select count(Salary) from employee_payroll");
 
         } catch (SQLException e) {
             e.printStackTrace();
@@ -10,5 +22,4 @@ import java.sql.*;
     public static void main(String[] args) throws SQLException {
         CreatedConnection();
     }
-
 }
