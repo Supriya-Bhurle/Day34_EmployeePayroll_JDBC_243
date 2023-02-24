30223684d61c6858cfad6ec13cca3cb722d41cd2
Supriya Bhurle <supriyabhurle28@gmail.com>
2023-02-24 10:54:28 +0530

    Added : sum avg min count oprations on DB

---
+++ src/main/java/com/bridgelabz/Employee.java
@@ -0,0 +1,19 @@
+package com.bridgelabz;
+
+import java.util.Date;
+
+public class Employee {
+    int  Id;
+    String Name;
+    double Salary   ;
+    Date Start_Date;
+    String Gender;
+    public Employee(int id, String name, double salary, Date start_Date, String gender) {
+        super();
+        Id = id;
+        Name = name;
+        Salary = salary;
+        Start_Date = start_Date;
+        Gender = gender;
+    }
+}
--- src/main/java/com/bridgelabz/EmployeePayroll.java
+++ src/main/java/com/bridgelabz/EmployeePayroll.java
@@ -1,39 +1,25 @@
 package com.bridgelabz;
 
-import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.DriverManager;
+import java.sql.*;
 import java.util.Enumeration;
 
 public class EmployeePayroll {
-    public static void main(String[] args) {
-        String jdbcURL =  "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";; // declare JdbcURL
-        String UserName = "root";
-        String Password = "Root";
+    public static void CreatedConnection() {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver"); // inbuilt method for Class.forName for loading driver
-            System.out.println("Driver loaded");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/payroll_service", "root",
+                    "Root");
+            Statement st = conn.createStatement();
+            st.execute("select sum(Salary) from employee_payroll");
+            st.execute("select avg(Salary) from employee_payroll");
+            st.execute("select min(Salary) from employee_payroll");
+            st.execute("select count(Salary) from employee_payroll");
 
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace(); // for tracing the exception
-
-        }
-        listDrivers();
-        try {
-            System.out.println("Connecting to Database...:" + jdbcURL); // for loading the drive for connect
-            Connection connection = DriverManager.getConnection(jdbcURL, UserName, Password); // creating object for
-            // connection
-            System.out.println("connection successful");
-        } catch (Exception e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    public static void listDrivers() {
-        Enumeration<Driver> driverList = DriverManager.getDrivers(); // 
-        while (driverList.hasMoreElements()) {
-            Driver driverClass = (Driver) driverList.nextElement();
-            System.out.println(" " + driverClass.getClass().getName());
-        }
+    public static void main(String[] args) throws SQLException {
+        CreatedConnection();
     }
 }
